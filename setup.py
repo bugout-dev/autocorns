4a17272c3187d5a7436e5f0f36eb01d480a5da14
Neeraj Kashyap <nkashy1@gmail.com>
2022-01-29 18:16:02 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+long_description = ""
+with open("README.md") as ifp:
+    long_description = ifp.read()
+
+setup(
+    name="autocorns",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=["eth-brownie", "tqdm", "web3"],
+    extras_require={
+        "dev": [
+            "black",
+            "moonworm >= 0.1.14",
+        ],
+        "distribute": ["setuptools", "twine", "wheel"],
+    },
+    description="Crypto Unicorns automation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="zomglings",
+    author_email="nkashy1@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "autocorns=autocorns.cli:main",
+        ]
+    },
+    include_package_data=True,
+)
