f02030985e47c9049f9c4c66786ce93fb86070ca
Neeraj Kashyap <nkashy1@gmail.com>
2022-01-29 18:19:08 -0800

    Added DarkForest CLI to autocorns

--- autocorns/cli.py
+++ autocorns/cli.py
@@ -1,5 +1,17 @@
+import argparse
+
+from . import DarkForest
+
 def main():
-    print("Hello world")
+    parser = argparse.ArgumentParser(description="autocorns: Crypto Unicorns automation")
+    parser.set_defaults(func=lambda _: parser.print_help())
+    subparsers = parser.add_subparsers()
+
+    dark_forest_parser = DarkForest.generate_cli()
+    subparsers.add_parser("dark-forest", parents=[dark_forest_parser], add_help=False)
+
+    args = parser.parse_args()
+    args.func(args)
 
 if __name__ == "__main__":
     main()
