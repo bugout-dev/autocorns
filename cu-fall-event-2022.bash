4fc5585b50d0f410490cb80da07eee0253c0751f
Neeraj Kashyap <nkashy1@gmail.com>
2022-11-15 08:44:34 -0800

    Added bash script to generate Fall Event 2022 leaderboard

---
+++ cu-fall-event-2022.bash
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+if [ -z "$BROWNIE_NETWORK" ]
+then
+    echo "Set BROWNIE_NETWORK environment variable"
+    exit 1
+fi
+
+if [ -z "$MOONSTREAM_ACCESS_TOKEN" ]
+then
+    echo "Set MOONSTREAM_ACCESS_TOKEN environment variable"
+    exit 1
+fi
+
+if [ -z "$CU_ADDRESS" ]
+then
+    echo "Set CU_ADDRESS environment variable"
+fi
+
+if [ -z "$DATA_DIR" ]
+then
+    echo "Set DATA_DIR environment variable"
+fi
+
+set -e
+
+TOTAL_SUPPLY=$(autocorns biologist total-supply --network $BROWNIE_NETWORK --address $CU_ADDRESS)
+
+echo "Total supply: $TOTAL_SUPPLY"
+
+time autocorns biologist dnas \
+    --network $BROWNIE_NETWORK \
+    --address $CU_ADDRESS \
+    --start 1 \
+    --end $TOTAL_SUPPLY \
+    --checkpoint "$DATA_DIR/dnas.json"
+
+time autocorns biologist mythic-body-parts \
+    --network $BROWNIE_NETWORK \
+    --address $CU_ADDRESS \
+    --dnas "$DATA_DIR/dnas.json" \
+    --checkpoint "$DATA_DIR/mythic-body-parts.json"
+
+time autocorns biologist stats \
+    --network $BROWNIE_NETWORK \
+    --address $CU_ADDRESS \
+    --dnas "$DATA_DIR/dnas.json" \
+    --checkpoint "$DATA_DIR/stats.json"
+
+time autocorns biologist moonstream-events \
+    -n breeding_hatching_leaderboard_events \
+    --start 1666828800 \
+    --end 1672531200 \
+    --interval 15 \
+    --max-retries 20 \
+    -o "$DATA_DIR/breeding_hatching_leaderboard_events.json"
+
+time autocorns biologist moonstream-events \
+    -n evolution_leaderboard_events \
+    --start 1666828800 \
+    --end 1672531200 \
+    --interval 15 \
+    --max-retries 20 \
+    -o "$DATA_DIR/evolution_leaderboard_events.json"
+
+time autocorns biologist fall-event-2022  \
+    --mythic-body-parts "$DATA_DIR/mythic-body-parts.json" \
+    --stats "$DATA_DIR/stats.json" \
+    --breeding-hatching-events "$DATA_DIR/breeding_hatching_leaderboard_events.json" \
+    --evolution-events "$DATA_DIR/evolution_leaderboard_events.json" \
+    >"$DATA_DIR/leaderboard.json"
+
+echo "Done!"
